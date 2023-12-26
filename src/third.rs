aabeb92f6f84ff96c132c7940c55ab13ddf87289
Bombwhale <solkblte@icloud.com>
2023-12-26 16:29:03 +0900

    3.2 Basics

--- src/third.rs
+++ src/third.rs
@@ -10,3 +10,87 @@ struct Node<T> {
     elem: T,
     next: Link<T>,
 }
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    // push 같은 거
+    pub fn prepend(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem,
+                next: self.head.clone(),
+            }))
+        }
+    }
+
+    // pop 같은 거
+    pub fn tail(&self) -> List<T> {
+        // map 대신 and_then:
+        // 인자로 넘기는 f에서 Option<>을 반환하기 때문에 map 쓰면 Option<Option<>>이 되어버림 
+        List { head: self.head.as_ref().and_then(|node| node.next.clone()) }
+    }
+
+    // peek 같은 거
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+}
+
+// Iter
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_deref() } // Rc 벗기기
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref(); // Rc 벗기기
+            &node.elem
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.prepend(1).prepend(2).prepend(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let list = List::new().prepend(1).prepend(2).prepend(3);
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+}
